194a87b1b150be2bdb2941be49fc0458ce3479ca
Itumeleng_Malematja <malematjai@gmail.com>
2023-06-21 13:14:56 -0700

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *add - adds two integers and returns the result
+  *@n: the first number to be computed
+  *@m: the second number to be computed
+  *Return:  integers value
+  */
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *times_table - function that prints the 9 times table
+  *starting with 0
+  *Return: Void
+  */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++);
+		{
+			product = i * j;
+
+			if(product < 10)
+			{
+				if(j > 1)
+				{
+					_putchar (32);
+				}
+				_putchar (product + '0');
+			}
+			else
+			{
+				_putchar ((product / 10) + '0');
+				_putchar ((product % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar (44);
+			}
+			_putchar (9);
+		}
+		_putchar (10);
+	}
+}
