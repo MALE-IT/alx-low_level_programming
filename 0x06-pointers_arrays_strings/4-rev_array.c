92f0acdf87d9e48233a09924fe10eaa9470a83ae
Itumeleng_Malematja <malematjai@gmail.com>
2023-07-01 14:10:53 -0700

    captize first word

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * reverse_array - reverses the content of an array of integers
  * @a: an array of integers
  * @n: the number of elements to swap
- *
- * Return: nothing.
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+  *cap_string - capitalizes all words of a string
+  *Separators of words: space, tabulation, new line
+  *@str: input string
+  *Return: Always 0 (Success)
+  */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+			continue;
+		}
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '\"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}')
+		{
+			++i;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 	'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+
+	return (0);
+}
