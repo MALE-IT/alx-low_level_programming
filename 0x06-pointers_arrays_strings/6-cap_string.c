8415e70e1095d7ede2a842ba50a1b35566c384e8
Itumeleng_Malematja <malematjai@gmail.com>
2023-07-01 15:07:09 -0700

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -38,13 +38,7 @@ char *cap_string(char *str)
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
-				continue;
 			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 	'Z')
-				str[i] = str[i] + 32;
 		}
 	}
 
