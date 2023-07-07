f83fd96237476358b91cd014ed8b1b6f284016d0
Itumeleng_Malematja <malematjai@gmail.com>
2023-07-06 17:35:47 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
+	r = wildcmp("main.c", "*.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*a*i*n*.*c*");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "main.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*c");
     printf("%d\n", r);
     r = wildcmp("main.c", "ma********************************c");
     printf("%d\n", r);
