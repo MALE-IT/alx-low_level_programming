3c719ea43bf76fd36e1687ee411eace17adde3fc
Itumeleng_Malematja <malematjai@gmail.com>
2023-08-07 11:44:50 -0700

    a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,27 +1,56 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 #include <fcntl.h>
+#include <unistd.h>
 
+/**
+  *read_textfile - reads a text file and prints it to the POSIX standard output
+  *@letters: is the number of letters it should read and print
+  *@filename: filename of letters to be read
+  *
+  *Return: actual number of letters it could read and print
+  *	if the file can not be opened or read, return 0
+  *	if filename is NULL return 0
+  *	if write fails or does not write the expected amount of bytes, return 0
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char c;
-	size_t count = 0;
-	FILE *fh;
+	char *buffer;
+	ssize_t w, t;
+	int fh;
 
 	if (filename == NULL)
-	return (0);
-
-	fh = fopen(filename, O_RDONLY);
-	if (fh == NULL)
-	return (0);
-
-	while ((c = fgetc(fh)) != EOF && count < letters)
 	{
-		_putchar(c);
-		count++;
+		return (0);
 	}
-
-	fclose(fh);
-
-	return (count);
+	fh = open(filename, O_RDONLY);
+	if (fh == -1)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fh);
+		return (0);
+	}
+	t = read(fh, buffer, letters);
+	if (t == -1)
+	{
+		close(fh);
+		free(buffer);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, t);
+	if (w == -1)
+	{
+		close(fh);
+		free(buffer);
+		return (0);
+	}
+	close(fh);
+	free(buffer);
+	return (w);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 #include <fcntl.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
