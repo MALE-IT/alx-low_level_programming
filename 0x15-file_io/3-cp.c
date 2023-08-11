25d1ba6da767056728ef7b4c5019a1544011586a
Itumeleng_Malematja <malematjai@gmail.com>
2023-08-11 15:14:02 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,46 +1,72 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 /**
   *main - copies the content of a file to another file.
-  *
-  *return  Return: 0 on success.
+  *@argc: argument count,number of argument passed on
+  *@argv: a pointer to array of argument
+  *Return: 0 on success.
   */
 int main(int argc, char *argv[])
 {
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
-	FILE *fptr1;
-	char c;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
+	int fd_from;
+	int fd_to;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-	fprintf(stderr,  "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 
-	fptr1 = fopen (file_from, O_RDONLY);
-		if (fptr1 == NULL)
+	fd_from = open(file_from, O_RDONLY);
+		if (fd_from == -1)
 		{
-			fprintf("Error: Can't read from file NAME_OF_THE_FILE");
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 			exit(98);
 		}
-	fptr2 = fopen("filename", "O_RDWR");
-		if (fptr2 ==NULL)
+	fd_to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY | 0644);
+		if (fd_to == -1)
 		{
-			dprintf("Error: Can't read from file NAME_OF_THE_FILE");
-			fclose(fptr1);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
 			exit(99);
 		}
-	while ((c = fgetc(fptr1)) != EOF)
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		fputc(c, fptr2);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+			exit(99);
+		}
 	}
-	dprintf("succefully copied\n");
-
-	fclose(fptr1);
-	fclose(fptr2);
-
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+	}
+	if (close(fd_from) == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't close fd %d\n",
+			fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+	dprintf(STDERR_FILENO,
+	"Error: Can't close fd %d\n",
+	fd_to);
+		 exit(100);
+	}
 	return (0);
 }
